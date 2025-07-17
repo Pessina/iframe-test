9278b3bd9729c237182671a285802f56f2b269b1
Felipe Pessina <fpessina@chorus.one>
2025-07-17 18:52:45 +0500

    Remove csp headers from parent

--- parent-app/next.config.ts
+++ parent-app/next.config.ts
@@ -4,24 +4,6 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          {
-            key: "X-Frame-Options",
-            value: "DENY",
-          },
-          {
-            key: "Content-Security-Policy",
-            value:
-              "frame-src https://iframe-app-phi.vercel.app http://localhost:3000; frame-ancestors 'self';",
-          },
-        ],
-      },
-    ];
-  },
 };
 
 export default nextConfig;
